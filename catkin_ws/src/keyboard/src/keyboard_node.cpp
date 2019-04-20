259f88eaaa9abc0e3861c2111498a332d41b8c1b
Oliver-ss <yuansongwx@outlook.com>
2019-04-20 02:07:37 -0400

    first commit

---
+++ catkin_ws/src/keyboard/src/keyboard_node.cpp
@@ -0,0 +1,223 @@
+#include <termios.h>
+#include <signal.h>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/poll.h>
+
+#include <boost/thread/thread.hpp>
+#include <ros/ros.h>
+#include <geometry_msgs/Twist.h>
+
+#define KEYCODE_UP 0x69 // I, Z axis up
+#define KEYCODE_LEFT 0x6A // J, Z rotate clockwise
+#define KEYCODE_RIGHT 0x6C // L, Z rotate anticlockwise
+#define KEYCODE_DOWN 0x6B // K, Z axis down
+
+#define KEYCODE_A_CAP 0x61 // Y axis up
+#define KEYCODE_D_CAP 0x64 // Y axis down
+#define KEYCODE_S_CAP 0x73 // X axis up
+#define KEYCODE_W_CAP 0x77 // X axis down
+
+class KeyboardTeleopNode
+{
+    private:
+        double walk_vel_;
+  //    double run_vel_;
+        double yaw_rate_;
+  //double yaw_rate_run_;
+        
+        geometry_msgs::Twist cmdvel_;
+        ros::NodeHandle n_;
+        ros::Publisher pub_;
+
+    public:
+        KeyboardTeleopNode()
+        {
+            pub_ = n_.advertise<geometry_msgs::Twist>("cmd_vel", 1);
+            
+            ros::NodeHandle n_private("~");
+            n_private.param("walk_vel", walk_vel_, 1.0);
+            n_private.param("yaw_rate", yaw_rate_, 1.0);
+        }
+        
+        ~KeyboardTeleopNode() { }
+        void keyboardLoop();
+        
+        void stopRobot()
+        {
+            cmdvel_.linear.x = 0.0;
+	    cmdvel_.linear.y = 0.0;
+	    cmdvel_.linear.z = 0.0;
+	    cmdvel_.angular.z = 0.0;
+            pub_.publish(cmdvel_);
+        }
+};
+
+KeyboardTeleopNode* tbk;
+int kfd = 0;
+struct termios cooked, raw;
+bool done;
+
+int main(int argc, char** argv)
+{
+    ros::init(argc,argv,"tbk", ros::init_options::AnonymousName | ros::init_options::NoSigintHandler);
+    KeyboardTeleopNode tbk;
+    
+    boost::thread t = boost::thread(boost::bind(&KeyboardTeleopNode::keyboardLoop, &tbk));
+    
+    ros::spin();
+    
+    t.interrupt();
+    t.join();
+    tbk.stopRobot();
+    tcsetattr(kfd, TCSANOW, &cooked);
+    
+    return(0);
+}
+
+void KeyboardTeleopNode::keyboardLoop()
+{
+    char c;
+    double max_tv = walk_vel_;
+    double max_rv = yaw_rate_;
+    bool dirty = false;
+    int x_speed = 0;
+    int y_speed = 0;
+    int z_speed = 0;
+    int z_turn = 0;
+    
+    
+    // get the console in raw mode
+    tcgetattr(kfd, &cooked);
+    memcpy(&raw, &cooked, sizeof(struct termios));
+    raw.c_lflag &=~ (ICANON | ECHO);
+    raw.c_cc[VEOL] = 1;
+    raw.c_cc[VEOF] = 2;
+    tcsetattr(kfd, TCSANOW, &raw);
+    
+    puts("Reading from keyboard");
+    puts("Use WASD keys to control the robot");
+    // puts("Press Shift to move faster");
+    
+    struct pollfd ufd;
+    ufd.fd = kfd;
+    ufd.events = POLLIN;
+    
+    for(;;)
+    {
+        boost::this_thread::interruption_point();
+        
+        // get the next event from the keyboard
+        int num;
+        
+        if ((num = poll(&ufd, 1, 250)) < 0)
+        {
+            perror("poll():");
+            return;
+        }
+        else if(num > 0)
+        {
+            if(read(kfd, &c, 1) < 0)
+            {
+                perror("read():");
+                return;
+            }
+        }
+        else
+        {
+            if (dirty == true)
+            {
+                stopRobot();
+                dirty = false;
+            }
+            
+            continue;
+        }
+        
+        switch(c)
+        {
+            case KEYCODE_UP:
+	      //max_tv = walk_vel_;
+                x_speed = 0;
+		y_speed = 0;
+		z_speed = 1;
+                z_turn = 0;
+                dirty = true;
+                break;
+            case KEYCODE_DOWN:
+	      // max_tv = walk_vel_;
+                x_speed = 0;
+		y_speed = 0;
+		z_speed = -1;
+                z_turn = 0;
+                dirty = true;
+                break;
+            case KEYCODE_LEFT:
+	      // max_rv = yaw_rate_;
+	        x_speed = 0;
+		y_speed = 0;
+		z_speed = 0;
+                z_turn = 1;
+                dirty = true;
+                break;
+            case KEYCODE_RIGHT:
+              //  max_rv = yaw_rate_;
+                x_speed = 0;
+		y_speed = 0;
+		z_speed = 0;
+                z_turn = -1;
+                dirty = true;
+                break;
+                
+            case KEYCODE_W_CAP:
+              //  max_tv = run_vel_;
+                x_speed = 1;
+		y_speed = 0;
+		z_speed = 0;
+                z_turn = 0;
+                dirty = true;
+                break;
+            case KEYCODE_S_CAP:
+              //  max_tv = run_vel_;
+                x_speed = -1;
+		y_speed = 0;
+		z_speed = 0;
+                z_turn = 0;
+                dirty = true;
+                break;
+            case KEYCODE_A_CAP:
+	      //max_rv = yaw_rate_run_;
+                x_speed = 0;
+		y_speed = 1;
+		z_speed = 0;
+                z_turn = 0;
+                dirty = true;
+                break;
+            case KEYCODE_D_CAP:
+	      // max_rv = yaw_rate_run_;
+                x_speed = 0;
+		y_speed = -1;
+		z_speed = 0;
+                z_turn = 0;
+                dirty = true;
+                break;
+                
+            default:
+	      //max_tv = walk_vel_;
+              //max_rv = yaw_rate_;
+                x_speed = 0;
+		y_speed = 0;
+		z_speed = 0;
+                z_turn = 0;
+                dirty = false;
+        }
+        
+        cmdvel_.linear.x = x_speed * max_tv;
+	cmdvel_.linear.y = y_speed * max_tv;
+	cmdvel_.linear.z = z_speed * max_tv;
+	cmdvel_.angular.z = z_turn * max_rv;
+	
+        pub_.publish(cmdvel_);
+    }
+}
---
+++ catkin_ws/src/opencvtest_py/src/test.py
@@ -0,0 +1,131 @@
+#!/usr/bin/env python
+from __future__ import print_function
+import time
+import roslib
+#roslib.load_manifest('my_package')
+import sys
+import rospy
+import cv2
+import glob
+import numpy as np
+import cv2.aruco as aruco
+from std_msgs.msg import String
+from sensor_msgs.msg import Image
+from std_msgs.msg import Float32MultiArray
+from cv_bridge import CvBridge, CvBridgeError
+
+class image_converter:
+
+  def __init__(self):
+    self.image_pub = rospy.Publisher("/cv_image",Image)
+    self.distance_pub = rospy.Publisher("/relative_distance",Float32MultiArray,queue_size=1)
+
+    self.bridge = CvBridge()
+    self.image_sub = rospy.Subscriber("/iris/image_raw",Image,self.callback)
+
+  def callback(self,data):
+    try:
+      # tranform ROS image message into opencv image
+      cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
+    except CvBridgeError as e:
+      print(e)
+
+    global  ret, mtx, dist, rvecs, tvecs
+
+    # aruco basic setting
+    aruco_dict = aruco.Dictionary_get(aruco.DICT_6X6_250)
+    parameters = aruco.DetectorParameters_create()
+
+    # convert the image 
+    cv_image_gray = cv2.cvtColor(cv_image, cv2.COLOR_BGR2GRAY)
+
+    # detect maker configuraiton
+    corners, ids, rejectedImgPoints = aruco.detectMarkers(cv_image_gray, aruco_dict, parameters=parameters)
+
+    if np.all(ids != None):
+      # pose estimation
+      # 0.19: markerLength, mtx: cameraMatrix, dist: distortion coefficients
+      # rvec: 
+      rvec, tvec,_ = aruco.estimatePoseSingleMarkers(corners[0], 0.19, mtx, dist)
+
+      # read corners informatio
+      topleftX = corners[0][0][0][0]
+      topleftY = corners[0][0][0][1]
+
+      toprightX = corners[0][0][1][0]
+      toprightY = corners[0][0][1][1]
+
+      bottomleftX = corners[0][0][2][0]
+      bottomlextY = corners[0][0][2][1]
+
+      bottomrightX = corners[0][0][3][0]
+      bottomrightY = corners[0][0][3][1]
+
+      # print corners information
+      #print("topleft  corner x {}".format(topleftX))
+      #print("topleft corner y {}".format(topleftY))
+
+      #print("topright corner x {}".format(toprightX))
+      #print("topright corner y {}".format(toprightY))
+
+     # print("bottomleft corner x {}".format(bottomleftX))
+     # print("bottomleft corner y {}".format(bottomlextY))
+
+     # print("bottomright corner x {}".format(bottomrightX))
+    #  print("bottomright corner y {}".format(bottomrightY))
+
+      # get pose information
+      cor_x = tvec[0][0][0]
+      cor_y = tvec[0][0][1]
+      cor_z = tvec[0][0][2]
+      print("x=",cor_x)
+      print("y=",cor_y)
+      print("z=",cor_z)
+#      time.sleep(2);#delete later
+      midpointX = (topleftX  + bottomrightX)/2
+      midpointY = (topleftY + bottomrightY)/2
+
+      # draw axis and detected marker
+      aruco.drawAxis(cv_image, mtx, dist, rvec[0], tvec[0], 0.1)
+      aruco.drawDetectedMarkers(cv_image, corners)
+
+      # draw ID text on top of image
+      font = cv2.FONT_HERSHEY_SIMPLEX
+      cv2.putText(cv_image, "Id: " + str(ids), (0,64), font, 1, (0,255,0),2,cv2.LINE_AA)
+
+      # incorporate pose information together and print on image
+      dis=Float32MultiArray()
+      #dis.layout=(3,1)
+      dis.data=(cor_x, cor_y, cor_z)
+
+      cv2.imshow("Image window", cv_image)
+      cv2.waitKey(3)
+
+      # Node Publish - pose information
+      self.distance_pub.publish(dis)
+
+      # Node Publish - cv_image
+      try:
+        self.image_pub.publish(self.bridge.cv2_to_imgmsg(cv_image, "bgr8"))
+      
+      except CvBridgeError as e:
+        print(e)
+
+def main(args):
+    global  ret, mtx, dist, rvecs, tvecs
+
+    # cameraMatrix and distortion coefficients
+    mtx = np.array([[552.1877667, 0. , 289.37921553], [ 0. , 550.98791255, 228.87373308], [0. , 0. , 1.]])
+    dist = np.array([[ 1.51623055e-03, 9.03278089e-02, 6.51492926e-03, -9.21777965e-05, -4.29890497e-01]])
+    
+    ic = image_converter()
+    rospy.init_node('image_converter', anonymous=True)
+
+    try:
+      rospy.spin()
+    except KeyboardInterrupt:
+      print("Shutting down")
+    cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    main(sys.argv)
